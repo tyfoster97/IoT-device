6a6f46423d6cae5953be8da2997b8467e63a9fdf
Ty Foster <tyfoster@outlook.com>
2020-10-16 16:19:21 -0700

    Modify setup

--- codeblocks-setup.sh
+++ codeblocks-setup.sh
@@ -1,7 +1,7 @@
 # install libs
 sudo apt install codeblocks-dev libwxgtk3.0-0v5 libwxgtk3.0-dev
 # clone simulavr-toolbar
-git -C ../../ clone https://github.com/dsandy12/simulavr-toolbar.git
+git -C ../ clone https://github.com/dsandy12/simulavr-toolbar.git
 # change directory
 cd ~/simulavr-toolbar
 # run codeblocks

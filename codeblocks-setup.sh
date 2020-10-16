77cbae2394f013d399d4a25705b61d0c4e6e64f3
Ty Foster <tyfoster@outlook.com>
2020-10-15 20:24:21 -0700

    Filename change

---
+++ codeblocks-setup.sh
@@ -0,0 +1,9 @@
+# install libs
+sudo apt install codeblocks-dev libwxgtk3.0-0v5 libwxgtk3.0-dev
+# clone simulavr-toolbar
+git -C ../../ clone https://github.com/dsandy12/simulavr-toolbar.git
+# change directory
+cd ~/simulavr-toolbar
+# run codeblocks
+codeblocks /na /nd --nosplashscreen -no-batch-window-close --rebuild ./toolbar.cbp --target="default"
+echo configure codeblocks
---
+++ headers-setup.sh
@@ -0,0 +1,3 @@
+# install linux headers
+sudo apt install linux-headers-$(uname -r) build-essential dkms
+echo install guest additions then run b-setup

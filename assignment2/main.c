e532b6658a18f57193d4508ad8ebbcf51165b00e
Ty Foster <tyfoster@outlook.com>
2020-10-22 17:25:20 -0700

    pseudocode skeleton

--- assignment2/main.c
+++ assignment2/main.c
@@ -1,14 +1,29 @@
-/*
+/**
+ * @author Ty Foster
+ * @version 2020.10.22
+ * SER486 - Assignment 2
  */
 
 #include <avr/io.h>
 
+void delay(unsigned long ms) {
+    //outer loop
+        //inner loop
+}
+
 int main(void)
 {
+    //pointer to DDRB
+    //pointer to PORTB
+    //pointer to PINB
+    //PORTB |= 0x02; //initialize pin 1 as output
 
-    // Insert code
 
     while(1)
+        // toggle pin
+        // wait 2s
+        // toggle pin
+        // wait 2s
     ;
 
     return 0;

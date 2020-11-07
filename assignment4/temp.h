020a7b3cdf5aa32cd055513ef44916df9b47f2d3
Ty Foster <tyfoster@outlook.com>
2020-11-06 18:01:36 -0800

    Initial commit

---
+++ assignment4/temp.h
@@ -0,0 +1,20 @@
+/********************************************************
+ * temp.h
+ *
+ * this file provides function declarations for SER486
+ * temp library functions.
+ *
+ * Author:   Ty Foster
+ * Date:    11/06/2020
+ * Revision: 1.0
+ * Course Number: SER486
+ * Assignment Number: 4
+ *
+ * Copyright(C) 2020, Arizona State University
+ * All rights reserved
+ */
+
+void temp_init(); /* initializes the temperature device */
+int temp_is_data_ready(); /* checks if the most recent conversion is complete */
+void temp_start(); /* starts conversion */
+signed int temp_get(); /* reads value from most recent conversion */

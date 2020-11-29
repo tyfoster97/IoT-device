a5de00ebd04d09fcf65ca2e589f2da57a3055aac
tyfoster97 <tyfoster@outlook.com>
2020-11-28 17:22:47 -0800

    Initial commit

---
+++ finp/main.c
@@ -0,0 +1,32 @@
+/********************************************************
+ * main.c
+ *
+ * SER486 Final Project
+ * Fall 2020
+ * @author Ty Foster (student)
+ * Instructor: Doug Sandy
+ * 
+ * this file implements functions for the SER486 final 
+ * project. TODO: describe purpose.
+ *
+ * functions:
+ *    main(void) - TODO: describe
+ */
+
+
+/*********************************************
+* main()
+*
+* TODO: describe main
+*/
+void main(void) {
+    
+    //TODO: implement code
+
+    while(1){
+        //TODO: control loop
+    }
+
+    /* theoretically unreachable code */
+    return 1;
+}
